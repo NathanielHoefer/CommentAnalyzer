c6d14b408fc6ce6dc224dab9b3d2d15656cef064
Nathaniel Hoefer <nhoefer146@gmail.com>
2017-06-13 09:37:53 -0500

    Comment header updated

--- CommentAnalyzer/comment.h
+++ CommentAnalyzer/comment.h
@@ -1,11 +1,38 @@
 #ifndef COMMENT_H
 #define COMMENT_H
 
+#include <QString>
+#include <QPair>
+#include <list>
 
 class Comment
 {
 public:
     Comment();
+    Comment(QString text, QPair lineNum, int colNum, bool badCom);
+    virtual ~Comment();
+
+    // Accessors
+    QString getText() const;
+    QPair getLineNumbers() const;
+    int getColumnNumber() const;
+    bool isBadComment() const;
+
+    // Mutators
+    void setBadComment(bool isBad);
+
+    /*  Combines the entered comment with the current one if they are
+     *  within the same column and the entered comment is on the next line.
+     *      Returns: The combined comment if conditions are met, otherwise
+     *          the calling comment is returned
+     */
+    Comment merge(const Comment &com);
+
+private:
+    QString mText;
+    QPair mLineNumbers;
+    int mColumnNumber;
+    bool mBadComment;
 };
 
 #endif // COMMENT_H
